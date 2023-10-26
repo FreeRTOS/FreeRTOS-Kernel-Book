41aa08d5dd6254fa03ad5f8d7ba4b6d19040fe8e
kar-rahul-aws <karahulx@amazon.com>
2023-10-26 20:55:00 +0530

    Update header in example 1

--- examples/Win32-simulator-MSVC/Examples/Example001/main.c
+++ examples/Win32-simulator-MSVC/Examples/Example001/main.c
@@ -1,21 +1,14 @@
 /*
- *  FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
- *  All rights reserved
+ *  This contains the Windows port implementation of the examples listed in the 
+ *  FreeRTOS book Mastering_the_FreeRTOS_Real_Time_Kernel.
  *
+ *  Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ *  SPDX-License-Identifier: MIT
+ * 
  *  VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
  *
  *  This file is part of the FreeRTOS distribution.
- *
- *  FreeRTOS is free software; you can redistribute it and/or modify it under
- *  the terms of the GNU General Public License (version 2) as published by the
- *  Free Software Foundation >>!AND MODIFIED BY!<< the FreeRTOS exception.
- *
- ***************************************************************************
- *  >>!   NOTE: The modification to the GPL is included to allow you to     !<<
- *  >>!   distribute a combined work that includes FreeRTOS without being   !<<
- *  >>!   obliged to provide the source code for proprietary components     !<<
- *  >>!   outside of the FreeRTOS kernel.                                   !<<
- ***************************************************************************
  *
  *  FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
  *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -30,11 +23,6 @@
  *  embedded software for free we request you assist our global community by
  *  participating in the support forum.
  *
- *  http://www.FreeRTOS.org/training - Investing in training allows your team to
- *  be as productive as possible as early as possible.  Now you can receive
- *  FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
- *  Ltd, and the world's leading authority on the world's leading RTOS.
- *
  *  http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
  *  including FreeRTOS+Trace - an indispensable productivity tool, a DOS
  *  compatible FAT file system, and our tiny thread aware UDP/IP stack.
@@ -42,15 +30,6 @@
  *  http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
  *  Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
  *
- *  http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
- *  Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
- *  licenses offer ticketed support, indemnification and commercial middleware.
- *
- *  http://www.SafeRTOS.com - High Integrity Systems also provide a safety
- *  engineered and independently SIL3 certified version for use in safety and
- *  mission critical applications that require provable dependability.
- *
- *  1 tab == 4 spaces!
  */
 
 /* FreeRTOS.org includes. */
